77196a553a05fea8569642f527bbe471d558cd27
Radwa Khaled <radwaabouzed@gmail.com>
2025-04-22 16:52:43 +0200

    add new data

--- DataStore/MyData.cs
+++ DataStore/MyData.cs
@@ -9,7 +9,7 @@ public static class MyData
             new Customer
             {
                 id = 1,
-                name = "Sara",
+                name = "Salim",
                 orders = new List<Order>
                 {
                     new Order
@@ -36,8 +36,51 @@ public static class MyData
                         date = DateTime.Now.AddDays(-5),
                         products = new List<Product>
                         {
-                            new Product { id = 3, name = "Keyboard", price = 80 },
+                            new Product { id = 3, name = "Tab", price = 800 },
                             new Product { id = 4, name = "Monitor", price = 300 }
+                        }
+                    }
+                }
+            },
+            new Customer
+            {
+                id = 3,
+                name = "Azza",
+                orders = new List<Order>
+                {
+                    new Order
+                    {
+                        id = 103,
+                        date = DateTime.Now.AddDays(-7),
+                        products = new List<Product>
+                        {
+                            new Product { id = 1, name = "Bed", price = 2500 },
+                            new Product { id = 1, name = "Lamp", price = 500 },
+                            new Product { id = 1, name = "Chair", price = 600 },
+                            new Product { id = 1, name = "Table", price = 900 },
+                            new Product { id = 1, name = "cups", price = 50 },
+                            new Product { id = 1, name = "Dishes", price = 80 },
+                            new Product { id = 2, name = "T-shirt", price = 20 }
+                        }
+                    }
+                }
+            },
+            new Customer
+            {
+                id = 4,
+                name = "Radwa",
+                orders = new List<Order>
+                {
+                    new Order
+                    {
+                        id = 104,
+                        date = DateTime.Now.AddDays(-8),
+                        products = new List<Product>
+                        {
+                            new Product { id = 1, name = "Laptop", price = 1800 },
+                            new Product { id = 2, name = "Mouse", price = 50 },
+                            new Product { id = 2, name = "keyboard", price = 70 },
+                            new Product { id = 2, name = "USB", price = 20 }
                         }
                     }
                 }
